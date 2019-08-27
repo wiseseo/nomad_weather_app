ea7609680ea748222b649aa79d3b3266b7405a6b
JihyeSeo1120 <mansesjh22@gmail.com>
2019-08-27 16:46:43 +0900

    loading page

--- App.js
+++ App.js
@@ -1,30 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { black } from 'ansi-colors';
+import Loading from './Loading';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-        <View style={styles.yellowView}></View>
-        <View style={styles.blueview}></View>
-    </View>
-  );
+  return <Loading />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-    /*
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',*/
-  },
-  yellowView: {
-    flex: 1,
-    backgroundColor: "yellow"
-  },
-  blueview: {
-    flex:3,
-    backgroundColor: "blue"
-  }
-});
+ 
